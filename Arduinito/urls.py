25eee88b6b24f594ab779a1b404d8fe0a22f5eec
AgusVelez5 <agustinmaurasvelez@gmail.com>
2017-09-26 13:30:36 -0300

    Sin errores

--- Arduinito/urls.py
+++ Arduinito/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('Arduinitos.urls', namespace = "ardu")),
-    url(r'^', include('ArduinitoApi.urls', namespace = "arduapi")),
+    url(r'', include('Arduinitos.urls', namespace='ardu' )),
+    url(r'', include('ArduinitoApi.urls', namespace='arduapi')),
 ]
 
--- Arduinitos/urls.py
+++ Arduinitos/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.views.generic import RedirectView
+from Arduinitos import views
 urlpatterns = [
             url(r'^$', RedirectView.as_view(url='home', permanent=False), name='root'),
-            url(r'^home$', 'Arduinitos.views.verhome', name='home' ),
+            url(r'^home/$', views.verhome , name='home' ),
             ]
