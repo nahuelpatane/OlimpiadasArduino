6940af36517cbed67def621a955557db8089172c
NahuelPatane <nahupatane@gmail.com>
2017-09-26 12:02:01 -0300

    urls added

--- Arduinitos/views.py
+++ Arduinitos/views.py
@@ -4,3 +4,6 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+def verhome(request):
+    return render_to_response('index.html',{},RequestContext(request))
